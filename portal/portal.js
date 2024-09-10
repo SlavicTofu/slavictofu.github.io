6ff71afd4e23ba5239a1ce9bb392755a563bc53d
SlavicTofu <80066554+SlavicTofu@users.noreply.github.com>
2024-09-09 22:10:57 -0400

    release of the portal

--- portal/portal.js
+++ portal/portal.js
@@ -1,11 +1,11 @@
 function submitFunction() {
 
-    let searchText = document.getElementById("name").value.trim();
+    let searchText = document.getElementById("form-search").value.trim();
     let form = document.getElementById('portal-form');
   
     if(searchText.length > 0) {
       document.getElementById("s").value = searchText;
-      form.action = "/search.html";
+      form.action = "/" + searchText + "/index.html";
       form.submit();
     } else {
       return false;
